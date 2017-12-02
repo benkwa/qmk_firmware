acff8831bb29430afad1f0b2b4e5c044649e5696
Ben Kwa <kenobi@google.com>
2017-12-02 09:50:03 -0800

    Make modifier and thumb clusters transparent

--- keyboards/ergodox_infinity/keymaps/kenobi/keymap.c
+++ keyboards/ergodox_infinity/keymaps/kenobi/keymap.c
@@ -88,11 +88,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [L_SYMB] = LAYOUT_ergodox(
                           // left hand
-                          KC_NO,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_NO,
-                          KC_NO,  KC_NO,    KC_AT,    KC_NO,    KC_EXLM,  KC_NO,  KC_TRNS,
-                          KC_NO,  KC_HASH,  KC_DLR,   KC_AMPR,  KC_ASTR,  KC_NO,   
-                          KC_NO,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_NO,  KC_TRNS,
-                          KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                          KC_NO,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_TRNS,
+                          KC_TRNS,  KC_NO,    KC_AT,    KC_NO,    KC_EXLM,  KC_NO,  KC_TRNS,
+                          KC_TRNS,  KC_HASH,  KC_DLR,   KC_AMPR,  KC_ASTR,  KC_NO,
+                          KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_NO,  KC_TRNS,
+                          KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
 
                                                                         KC_TRNS,  KC_TRNS,
                                                                                   KC_TRNS,
@@ -102,8 +102,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_NO,   KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10, KC_NO,
                           KC_NO,   KC_NO,    KC_LCBR,  KC_RCBR,  KC_NO,   KC_NO,  KC_NO,
                                    KC_MINS,  KC_LPRN,  KC_RPRN,  KC_NO,   KC_NO,  KC_NO,
-                          KC_TRNS, KC_NO,    KC_LBRC,  KC_RBRC,  KC_NO,   KC_NO,  KC_NO,
-                                             KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO,
+                          KC_TRNS, KC_NO,    KC_LBRC,  KC_RBRC,  KC_NO,   KC_NO,  KC_TRNS,
+                                             KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_TRNS,
 
                           KC_TRNS, KC_TRNS,
                           KC_TRNS,
@@ -133,26 +133,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [L_NPAD] = LAYOUT_ergodox(
                           // left hand
-                          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
 
-                                                                  KC_NO,  KC_NO,
-                                                                          KC_NO,
-                                                          KC_NO,  KC_NO,  KC_NO,
+                                                               KC_TRNS,  KC_TRNS,
+                                                                         KC_TRNS,
+                                                     KC_TRNS,  KC_TRNS,  KC_TRNS,
 
                           // right hand
-                          KC_TRNS, KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,
-                          KC_NO, KC_NO,  KC_7,   KC_8,   KC_9,    KC_NO,  KC_NO,
-                                 KC_NO,  KC_4,   KC_5,   KC_6,    KC_NO,  KC_NO,
-                          KC_NO, KC_NO,  KC_1,   KC_2,   KC_3,    KC_NO,  KC_NO,
-                                 KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,
+                          KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_7,   KC_8,   KC_9,    KC_NO,  KC_NO,
+                                    KC_NO,  KC_4,   KC_5,   KC_6,    KC_NO,  KC_NO,
+                          KC_NO,    KC_NO,  KC_1,   KC_2,   KC_3,    KC_NO,  KC_NO,
+                                            KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
        
-                          KC_NO, KC_NO,
-                          KC_NO,
-                          KC_NO, KC_NO, KC_0
+                          KC_TRNS,  KC_TRNS,
+                          KC_TRNS,
+                          KC_TRNS,  KC_TRNS,  KC_0
                           ),
 };
 
